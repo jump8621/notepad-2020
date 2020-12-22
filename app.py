0b7893ae7683494d801fc84c04fa3b537e426eac
Juliann Pezzullo <jump8621@gmail.com>
2020-12-22 17:11:10 -0500

    add postgres

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///notepad.sqlite')
+
+db = SQLAlchemy(app)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
 
 @app.route("/")
 def index():
     return "Priscilla Mullins(1600-1651) - 10th Great Grandmother - Mayflower"
+
+@app.route('/api/tasks-postgres')
+def tasksPostGres():
+    tasks = db.session.query(Task)
+    data = []
+
+    for task in tasks:
+        item = {
+            'id': task.id,
+            'description': task.description
+        }
+        data.append(item)
+
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
